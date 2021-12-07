9cd67f17364fa65ba783b5a2a70c6b4cab89bf4e
Rob <iamrobertjeffrey@gmail.com>
2021-12-07 20:35:57 +0000

    aligned product text in the middle

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -19,7 +19,7 @@ const Product = ({ current }) =>
 			h="500px">
 			<img style={{ objectFit: "cover", height: "50%", width: "100%" }} alt="clothing" key={current.id} src={current.image} />
 
-			<Box display="flex" p="5" isTruncated justifyContent="space-between">{current.product_name} <Button colorScheme="blue">{current.sex === "male" && "Men"}{current.sex === "female" && "Women"}{current.sex === "unisex" && "Unisex"}</Button></Box>
+			<Box alignItems="center" display="flex" p="5" isTruncated justifyContent="space-between">{current.product_name} <Button colorScheme="blue">{current.sex === "male" && "Men"}{current.sex === "female" && "Women"}{current.sex === "unisex" && "Unisex"}</Button></Box>
 			<Box flexGrow="1" display="flex" p="5"><Text height="3rem" fontSize="sm" noOfLines="2" >{current.product_description}</Text></Box>
 			<Box justifyContent="space-between" display="flex" p="5">&#163;{current.product_price} <Button colorScheme="teal">Add To Basket</Button></Box>
 
