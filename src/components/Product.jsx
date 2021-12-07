e31f8b168985d61495fc0d8baf1526d51c21dcde
Rob <iamrobertjeffrey@gmail.com>
2021-12-07 19:36:10 +0000

    added homepage description

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,14 +2,24 @@ import { Button } from '@chakra-ui/button'
 import { Box } from '@chakra-ui/layout'
 import React from 'react'
 import { Text } from '@chakra-ui/layout'
+import { useColorMode } from '@chakra-ui/color-mode'
 
 const Product = ({ current }) =>
 {
+	const { colorMode } = useColorMode();
 	return (
-		<Box display="flex" boxSizing="border-box" flexDirection="column" bg="gray.700" rounded="lg" overflow="hidden" shadow="lg" w="100%" h="500px">
+		<Box display="flex"
+			boxSizing="border-box"
+			flexDirection="column"
+			bg={colorMode === "light" ? "gray.200" : "gray.700"}
+			rounded="lg"
+			overflow="hidden"
+			shadow="lg"
+			w="100%"
+			h="500px">
 			<img style={{ objectFit: "cover", height: "50%", width: "100%" }} alt="clothing" key={current.id} src={current.image} />
 
-			<Box display="flex" p="5" isTruncated justifyContent="space-between">{current.product_name} <Button colorScheme="teal">{current.sex === "male" && "Men"}{current.sex === "female" && "Women"}{current.sex === "unisex" && "Unisex"}</Button></Box>
+			<Box display="flex" p="5" isTruncated justifyContent="space-between">{current.product_name} <Button colorScheme="blue">{current.sex === "male" && "Men"}{current.sex === "female" && "Women"}{current.sex === "unisex" && "Unisex"}</Button></Box>
 			<Box flexGrow="1" display="flex" p="5"><Text height="3rem" fontSize="sm" noOfLines="2" >{current.product_description}</Text></Box>
 			<Box justifyContent="space-between" display="flex" p="5">&#163;{current.product_price} <Button colorScheme="teal">Add To Basket</Button></Box>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import Product from '../components/Product'
 import Masonry from 'react-masonry-css'
 import '../style.css'
+import { Text } from '@chakra-ui/layout'
 
 const Home = ({ supabase, products, setProducts }) =>
 {
@@ -16,6 +17,7 @@ const Home = ({ supabase, products, setProducts }) =>
 
 	useEffect(() =>
 	{
+
 		async function getProducts()
 		{
 			const products = await supabase.from("product")
@@ -26,13 +28,20 @@ const Home = ({ supabase, products, setProducts }) =>
 	}, [supabase, setProducts])
 
 	return (
-		<Container display="flex" justifyContent="center" width="100%" maxW="100%">
+		<Container display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" width="100%" maxW="100%">
+			<Text fontSize="7xl">Welcome to Rob's shop</Text>
+			<Container p="2rem" width="60%" maxW="60%">
+				<Text textAlign="left">This is a fake shopping website for the purpose of demonstrating what I can make. <b> Nothing on this website can actually be purchased.</b> You can register a new account, login, logout, delete your account, change your login details, browse my fake catalog and add items to your basket. Below are three examples of items you can find in my catalog.</Text>
+			</Container>
 			<Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid"
 				columnClassName="my-masonry-grid_column">
 				{
 
 					products.map((current) => (
-						<Product key={current.id} current={current} />
+
+						current.id < 4 && (<Product key={current.id} current={current} />)
+
+
 					))}
 
 			</Masonry>
